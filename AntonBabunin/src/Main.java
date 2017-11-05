e36734369487c09dd81c22342fac17e6f015726c
if210 <if2102@outlook.com>
2017-11-05 17:15:35 +0200

    Result after classwork

--- AntonBabunin/src/Main.java
+++ AntonBabunin/src/Main.java
@@ -1,5 +1,26 @@
+import artcode.Bill;
+import artcode.Product;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Class \"Main\" was created");
+
+        Product notebook = new Product("Notebook", 10000.99,1);
+        Product smartfone = new Product("Smartphone", 5000.99,2);
+
+        Bill firstBill = new Bill();
+        firstBill.setProducts();
+        firstBill.addProduct(notebook);
+        firstBill.addProduct(smartfone);
+
+        firstBill.delFromBill(smartfone);
+        firstBill.delFromBill(smartfone);
+        firstBill.closeBill();
+
+
+
+        firstBill.addProduct(notebook);
+
+        System.out.printf("Summ %.2f", firstBill.getAmountPrice());
+
     }
 }
