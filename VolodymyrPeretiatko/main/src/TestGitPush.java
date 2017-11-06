0eb90aa6a76a226a083bdc5a9162582fd487bbd1
vperetyatko <voc@ukr.net>
2017-11-06 17:36:39 +0200

    added model (Clases -> Bill, Product, Salesman, Terminal)

--- VolodymyrPeretiatko/main/src/TestGitPush.java
+++ VolodymyrPeretiatko/main/src/TestGitPush.java
@@ -1,6 +1,4 @@
-/**
- * Created by vperetyatko on 06.11.2017.
- */
+
 public class TestGitPush {
     public static void main(String[] args) {
         System.out.println("bip bip");
---
+++ VolodymyrPeretiatko/main/src/hw1/Bill.java
@@ -0,0 +1,17 @@
+package hw1;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class Bill {
+
+    private int id;
+    private ArrayList<Product> products;
+    private Salesman salesman;
+    private int amountPrice;
+    private Date closeTime;
+
+
+
+}
