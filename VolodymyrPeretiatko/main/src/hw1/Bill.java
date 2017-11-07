073c4edf59c9beffa234cb54f04e01126c421276
vperetyatko <voc@ukr.net>
2017-11-07 16:50:06 +0200

    added tests

--- VolodymyrPeretiatko/main/src/hw1/Bill.java
+++ VolodymyrPeretiatko/main/src/hw1/Bill.java
@@ -9,9 +9,83 @@ public class Bill {
     private int id;
     private ArrayList<Product> products;
     private Salesman salesman;
-    private int amountPrice;
+    private double amountPrice;
     private Date closeTime;
 
+    public static Bill openBill(int id, Salesman salesman){
+        Bill bill = new Bill(id, salesman);
+        bill.products = new ArrayList<>();
+        return bill;
+    }
 
+    public static Bill closeBill(Bill bill){
+        bill.setCloseTime(new Date(System.currentTimeMillis()));
+        bill.calculateAmountPrice();
+        return bill;
+    }
+
+
+    public boolean addProduct(Product p){
+        this.amountPrice += p.getPrice();
+        return this.products.add(p);
+    }
+
+    private void calculateAmountPrice(){
+        for (Product p : this.products){
+            amountPrice += p.getPrice();
+        }
+    }
+
+    public String printBill(){
+        return this.toString();
+    }
+
+    public Bill(int id, Salesman salesman) {
+        this.id = id;
+        this.salesman = salesman;
+    }
+
+    public void setSalesman(Salesman salesman) {
+        this.salesman = salesman;
+    }
+
+    public void setAmountPrice(double amountPrice) {
+        this.amountPrice = amountPrice;
+    }
+
+    public void setCloseTime(Date closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public Salesman getSalesman() {
+        return salesman;
+    }
+
+    public double getAmountPrice() {
+        return amountPrice;
+    }
+
+    public Date getCloseTime() {
+        return closeTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "id=" + id +
+                ", products=" + products +
+                ", salesman=" + salesman +
+                ", amountPrice=" + amountPrice +
+                ", closeTime=" + closeTime +
+                '}';
+    }
 
 }
--- VolodymyrPeretiatko/main/src/hw1/Product.java
+++ VolodymyrPeretiatko/main/src/hw1/Product.java
@@ -4,9 +4,12 @@ public class Product {
 
     private int id;
     private String name;
+    private double price;
 
-    public Product(String name) {
+    public Product(int id, String name, double price) {
+        this.id = id;
         this.name = name;
+        this.price = price;
     }
 
     @Override
@@ -30,7 +33,11 @@ public class Product {
 
     @Override
     public String toString() {
-        return "Product{" + "id=" + id + ' ' + name + '}';
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
     }
 
     public void printFullInfo(){
@@ -49,5 +56,13 @@ public class Product {
 
     public String getName() {
         return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
