ed1778fd379e41ec4c484726ca3ea33f6ad5ddf7
SmooZzzie <artemshchepets@gmail.com>
2017-11-07 09:59:17 +0200

    Added method addProduct() and closeAndSaveBill() to class Terminal. Changed some logic of array pointers.

--- ArtemShchepets/src/week1/data/Terminal.java
+++ ArtemShchepets/src/week1/data/Terminal.java
@@ -8,10 +8,10 @@ public class Terminal {
     private Seller[] sellers;
 
     private int actualSizeOfBills;
-    private int getActualSizeOfSellers;
+    private int actualSizeOfSellers;
 
     private int currentSellerIndex = -1;
-    private int currentBillIndex = -1;
+    private int currentBillIndex = 1;
 
     boolean isSignIn = false;
 
@@ -43,8 +43,8 @@ public class Terminal {
         return actualSizeOfBills;
     }
 
-    public int getGetActualSizeOfSellers() {
-        return getActualSizeOfSellers;
+    public int getActualSizeOfSellers() {
+        return actualSizeOfSellers;
     }
 
     public void signIn(String login, String password) {
@@ -53,7 +53,7 @@ public class Terminal {
             System.out.println("Enter login\\pass correctly!");
         } else {
 
-            for (int i = 0; i < getActualSizeOfSellers; i++) {
+            for (int i = 0; i < actualSizeOfSellers; i++) {
                 if (sellers[i].getLogin().equals(login) && sellers[i].getPassword().equals(password)) {
                     isSignIn = true;
                     currentSellerIndex = i;
@@ -70,18 +70,23 @@ public class Terminal {
 
     public void createBill() {
 
+        // check if salesman is signed in
         if (!isSignIn) {
             System.out.println("Firstly, you should sign in!");
         } else {
 
             Bill newBill = new Bill(sellers[currentSellerIndex]);
 
+            // set actual id for new bill
             newBill.setId(actualSizeOfBills + 1);
 
             Scanner scanner = new Scanner(System.in);
 
-            boolean toContinue = false;
+            boolean toContinue; // always false?
+
+            // fill the list of products in new bill
             do {
+                // TODO check valid input
                 System.out.println("Enter product name: ");
                 String newProductName = scanner.next();
 
@@ -98,21 +103,71 @@ public class Terminal {
                 toContinue = ("y".equals(scanner.next()));
 
             } while (toContinue);
-
+            // TODO check valid input
+            // set close time for the bill
             System.out.println("Set close time. Like this --> 12:12:12");
             String parsingTime = scanner.next();
 
             String[] parsedTime = parsingTime.split(":");
 
             newBill.getTime().setHours(Integer.decode(parsedTime[0]));
-            newBill.getTime().setHours(Integer.decode(parsedTime[1]));
-            newBill.getTime().setHours(Integer.decode(parsedTime[2]));
+            newBill.getTime().setMinutes(Integer.decode(parsedTime[1]));
+            newBill.getTime().setSeconds(Integer.decode(parsedTime[2]));
 
-
+            //add new bill to the terminal
             bills[actualSizeOfBills] = newBill;
             actualSizeOfBills++;
+            currentBillIndex = actualSizeOfBills - 1;
 
+            System.out.println("***BILL IS CREATED***");
         }
+    }
+
+    public void addProductToBill() {
+
+        if (!isSignIn) {
+
+            System.out.println("Firstly, you should sign in!");
+
+        } else {
+
+            if (currentBillIndex == -1) {
+
+                System.out.println("There aren't any bills in terminal!");
+
+            } else {
+
+                if (bills[actualSizeOfBills - 1].isClosed()) {
+
+                    System.out.println("You can't add products. Bill is closed!");
+
+                } else {
+
+                    Scanner scanner = new Scanner(System.in);
+
+                    System.out.println("Enter product name: ");
+                    String newProductName = scanner.next();
+
+                    System.out.println("Enter product price: ");
+                    double newProductPrice = scanner.nextDouble();
+
+                    System.out.println("Enter product code: ");
+                    String newProductCode = "#" + scanner.next();
+
+                    Product newProduct = new Product(newProductName, newProductPrice, newProductCode);
+
+                    bills[actualSizeOfBills - 1].addProduct(newProduct);
+                }
+            }
+        }
+    }
+
+    // why do we need such method in Terminal if we already had one in Bill?
+
+    public void closeAndSaveBill() {
+
+        bills[currentBillIndex].closeBill();
+
     }
 
 }
