807b9dbcda40b2c0793194b209d88e32cd3edd72
smikhun <a.smikhun@gmail.com>
2017-11-07 19:09:10 +0200

    added method to class Terminal

--- AndriySmikhun/src/Salesman.java
+++ AndriySmikhun/src/Salesman.java
@@ -2,14 +2,45 @@ public class Salesman {
 
     String fullname;
     String login;
-    String Password;
+    String password;
+    boolean status;
 
-    public Salesman(String fullname, String login, String password) {
+    public Salesman(String fullname, String login, String password, boolean status) {
         this.fullname = fullname;
         this.login = login;
-        Password = password;
+        this.password = password;
+        this.status = status;
+
     }
 
     public Salesman() {
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
--- AndriySmikhun/src/Terminal.java
+++ AndriySmikhun/src/Terminal.java
@@ -11,13 +11,21 @@ public class Terminal {
     public Terminal() {
     }
 
-    public void login(){};                               //enter login and password
-    public void createBill(){};                          //create and open bill
-    public void addProduct(){};                          //
-    public void closeAndSaveBill(){};                    //close and save bill in toarray
-    public void findBillById(){};                        //search bill
-    public void findSalesmanByLoginOrFullname(){};       //search SAleman whith data
-    public void getTopNofSalesMan(){};                   //
-    public void doSomeStatisticStuff(){};                //statistic about products and bill
+    public void login(String login, String password){
+        for (int i = 0; i < sales.length; i++){
+            if (sales[i].getLogin().equals(login) && sales[i].getLogin().equals(password)){
+                sales[i].setStatus(true);
+            }else {
+                System.out.println("Wrong login or password.");
+            }
+        }
+    }                               //enter login and password
+    public void createBill(){}                          //create and open bill
+    public void addProduct(){}                          //
+    public void closeAndSaveBill(){}                    //close and save bill in toarray
+    public void findBillById(){}                        //search bill
+    public void findSalesmanByLoginOrFullname(){}       //search SAleman whith data
+    public void getTopNofSalesMan(){}                   //
+    public void doSomeStatisticStuff(){}                //statistic about products and bill
 
 }
