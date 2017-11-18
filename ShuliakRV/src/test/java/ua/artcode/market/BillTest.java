26aa9f808ffabb49521e0b000365d16cd1e9e1ef
ShuliakRV <okej@yandex.ua>
2017-11-19 01:12:40 +0200

    add interface ITerminal

--- ShuliakRV/src/test/java/ua/artcode/market/BillTest.java
+++ ShuliakRV/src/test/java/ua/artcode/market/BillTest.java
@@ -6,6 +6,12 @@ import ua.artcode.market.models.*;
 import static org.junit.Assert.*;
 
 public class BillTest {
+    @Test
+    public void addProduct() throws Exception {
+        AppDB appDB = new AppDB();
+        Bill b = new Bill(appDB.getSales()[0]);
+        assertTrue(b.addProduct(appDB.getProducts()[0]));
+    }
 
     @Test
     public void getId() throws Exception {
@@ -33,13 +39,14 @@ public class BillTest {
         Bill b = new Bill(appDB.getSales()[0]);
         b.addProduct(appDB.getProducts()[0]);
         b.closeBill();
-        assertEquals(appDB.getProducts()[0].getPrice(),b.getAmountPrice(),0.1);
+        assertEquals(appDB.getProducts()[0].getPrice(), b.getAmountPrice(), 0.1);
     }
 
     @Test
     public void closeBill() throws Exception {
         AppDB appDB = new AppDB();
-        Bill b = new Bill(appDB.getSales()[0]);;
+        Bill b = new Bill(appDB.getSales()[0]);
+        ;
         b.addProduct(appDB.getProducts()[0]);
         b.addProduct(appDB.getProducts()[1]);
         assertTrue(b.closeBill());
