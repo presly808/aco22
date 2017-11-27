38959197a43122d1a5403554b1575163b4a01415
ilya.volkov <illiamvolkov@gmail.com>
2017-11-27 13:51:12 +0200

    ArrayList test OK

--- IlliaVolkov/src/main/java/ua/artcode/market/controllers/BillController.java
+++ IlliaVolkov/src/main/java/ua/artcode/market/controllers/BillController.java
@@ -6,9 +6,7 @@ import ua.artcode.market.model.Product;
 import ua.artcode.market.model.ProductBill;
 import ua.artcode.market.view.InterfaceServices;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Date;
 
 public class BillController implements IBill{
@@ -17,37 +15,11 @@ public class BillController implements IBill{
 
         boolean productProcessed = false;
 
-        //usage matrix
-        /*for (int i = 0; i < currentBill.getProducts().length; i++) {
+        for (ProductBill itemProductBill: currentBill.getProductsBill()) {
 
-            if ((currentBill.getProducts()[i][0] == 0 || currentBill.getProducts()[i][0] == productCode) && !productProcessed) {
+            if (itemProductBill.getProductCode() == productCode && !productProcessed) {
 
-                currentBill.setQuantityGoods((currentBill.getProducts()[i][0] == 0 ? (currentBill.getQuantityGoods() + 1) : currentBill.getQuantityGoods()));
-
-                currentBill.setProducts(i,0, productCode);
-                currentBill.setProducts(i,1, quantity + currentBill.getProducts()[i][1]);
-
-                currentBill.setAmountPrice(currentBill.getAmountPrice()
-                        + Math.rint(Product.findByCode( currentBill.getProductList(), productCode).price*currentBill.getProducts()[i][1]*100)/100);
-
-                productProcessed = true;
-            }
-        }
-
-        if (!productProcessed) {
-            System.out.println("The limit of the number of products in the Check has been reached " +
-                    currentBill.getProducts().length + " pieces");
-        }*/
-
-        //usage ArrayList
-        ArrayList<ProductBill> currentProductsBill = currentBill.getProductsBill();
-        for (int i = 0; i < currentProductsBill.size(); i++) {
-
-
-            ProductBill currentProductBill = currentProductsBill.get(i);
-            if (currentProductBill.getProductCode() == productCode && !productProcessed) {
-
-                currentProductBill.setProductQuontity(currentProductBill.getProductQuontity() + quantity);
+                itemProductBill.setProductQuontity(itemProductBill.getProductQuontity() + quantity);
 
                 calculateAmountPrice(currentBill);
 
@@ -56,7 +28,7 @@ public class BillController implements IBill{
         }
 
         if (!productProcessed) {
-            currentBill.setProductsBill(new ProductBill(Product.findByCode( currentBill.getProductList(), productCode).code, quantity));
+            currentBill.setProductsBill(new ProductBill(Product.findByCode( currentBill.getProductsPrice(), productCode).code, quantity));
 
             calculateAmountPrice(currentBill);
             currentBill.setQuantityGoods(currentBill.getQuantityGoods() + 1);
@@ -67,47 +39,19 @@ public class BillController implements IBill{
 
         boolean productProcessed = false;
 
-        //usage matrix
-        /*for (int i = 0; i < currentBill.getProducts().length; i++) {
+        List<ProductBill> currentProductsBill = currentBill.getProductsBill();
+        for (ProductBill itemProductBill: currentProductsBill) {
 
-            if (currentBill.getProducts()[i][0] == productCode && !productProcessed) {
-
-                if (quantity == 0) {
-                    currentBill.setProducts(i,0, 0);
-                    currentBill.setProducts(i,1, 0);
-                    currentBill.setQuantityGoods(currentBill.getQuantityGoods() - 1);
-
-                    calculateAmountPrice(currentBill);
-
-                    productProcessed = true;
-                }
-                else {
-                    currentBill.setProducts(i,0, productCode);
-                    currentBill.setProducts(i,1, quantity);
-
-                    calculateAmountPrice(currentBill);
-
-                    productProcessed = true;
-                }
-            }
-        }*/
-
-        //usage ArrayList
-        ArrayList<ProductBill> currentProductsBill = currentBill.getProductsBill();
-        for (int i = 0; i < currentProductsBill.size(); i++) {
-
-
-            ProductBill currentProductBill = currentProductsBill.get(i);
-            if (currentProductBill.getProductCode() == productCode && !productProcessed) {
+            if (itemProductBill.getProductCode() == productCode && !productProcessed) {
 
                 if (quantity == 0) {
 
-                    currentProductsBill.remove(i);
+                    currentProductsBill.remove(itemProductBill);
                     currentBill.setQuantityGoods(currentBill.getQuantityGoods() - 1);
                     calculateAmountPrice(currentBill);
                 }
                 else {
-                    currentProductBill.setProductQuontity(quantity);
+                    itemProductBill.setProductQuontity(quantity);
                     calculateAmountPrice(currentBill);
                 }
                 productProcessed = true;
@@ -121,46 +65,32 @@ public class BillController implements IBill{
         InterfaceServices.choseProduct(currentBill);
     }
 
-    public static String getBillInfoForPrint(Bill currentBill){
+    public static String getBillHeadInfoForPrint(Bill currentBill){
 
         String message = "Bill №" + currentBill.getCode() +
                 "/ quontity of goods - " + currentBill.getQuantityGoods() +
                 "/ Amount - " + currentBill.getAmountPrice() +
-                "/  SalesMan - " + currentBill.getSalesMan().getFullName() +
+                "/ SalesMan - " + currentBill.getSalesMan().getFullName() +
                 "/ Closed - " + currentBill.closed +
-                "/ CloseTime - " + currentBill.closeTime;
+                "/ createTime - " + (currentBill.createTime == null ? "" : currentBill.createTime) +
+                "/ CloseTime - " + (currentBill.closeTime == null ? "" : currentBill.closeTime);
 
         return message;
     }
 
-    public static String GetProductsForPrint(Bill currentBill){
+    public static String GetBillProductsForPrint(Bill currentBill){
 
         String message = "Code\t"+"Goods\t"+"Price\t"+"Quantity\n";
 
-        //usage Matrix
-        /*for (int i = 0; i < currentBill.getProducts().length; i++) {
+        for (ProductBill itemProductBill: currentBill.getProductsBill()) {
 
-            if (currentBill.getProducts()[i][0] != 0) {
+            Product currentProduct = Product.findByCode(currentBill.getProductsPrice(),
+                    itemProductBill.getProductCode());
 
-                Product currentProduct = Product.findByCode(currentBill.getProductList(), currentBill.getProducts()[i][0]);
-                message += "" + currentProduct.code +
-                        "\t\t" + currentProduct.name +
-                        "\t\t" + currentProduct.price +
-                        "\t\t" + currentBill.getProducts()[i][1]+"\n";
-            }
-        }*/
-
-        //ArrayLis
-        ArrayList<ProductBill> currentProductsBill = currentBill.getProductsBill();
-        for (int i = 0; i < currentProductsBill.size(); i++) {
-
-            ProductBill currentProductBill = currentProductsBill.get(i);
-
-            Product currentProduct = Product.findByCode(currentBill.getProductList(), currentProductBill.getProductCode());
             message += "" + currentProduct.code +
                     "\t\t" + currentProduct.name +
                     "\t\t" + currentProduct.price +
-                    "\t\t" + currentProductBill.getProductQuontity()+"\n";
+                    "\t\t" + itemProductBill.getProductQuontity()+"\n";
         }
 
         return message;
@@ -170,25 +100,13 @@ public class BillController implements IBill{
 
         currentBill.setAmountPrice(0);
 
-        //usage Matrix
-        /*for (int i = 0; i < currentBill.getProducts().length; i++) {
+        for (ProductBill itemProductBill: currentBill.getProductsBill()) {
 
-
-            if (currentBill.getProducts()[i][0] != 0) {
-                Product currentProduct = Product.findByCode(currentBill.getProductList(), currentBill.getProducts()[i][0]);
-                currentBill.setAmountPrice(Math.rint(currentBill.getAmountPrice() + currentProduct.price*currentBill.getProducts()[i][1]*100)/100);
-            }
-        }*/
-
-        //ArrayList
-        for (int i = 0; i < currentBill.getProductsBill().size(); i++) {
-
-            ProductBill currentProductBill = currentBill.getProductsBill().get(i);
-
-            Product currentProduct = Product.findByCode(currentBill.getProductList(), currentProductBill.getProductCode());
+            Product currentProduct = Product.findByCode(currentBill.getProductsPrice(),
+                    itemProductBill.getProductCode());
 
             currentBill.setAmountPrice(Math.rint(currentBill.getAmountPrice()
-                    + currentProduct.price*currentProductBill.getProductQuontity()*100)/100);
+                    + currentProduct.price*itemProductBill.getProductQuontity()*100)/100);
         }
     }
 
