d793bd589a482ce84ae88bfe928b4d46e45d658f
SmooZzzie <artemshchepets@gmail.com>
2017-12-04 17:14:46 +0200

    added some coverage

--- ArtemShchepets/src/main/java/taskJava8/model/User.java
+++ ArtemShchepets/src/main/java/taskJava8/model/User.java
@@ -12,9 +12,6 @@ public class User {
    private int company;
    private int age;
 
-    public User() {
-    }
-
     public User(long id, String name, int age, String sex, double salary, String city, int company) {
         this.id = id;
         this.name = name;
@@ -29,32 +26,12 @@ public class User {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSex() {
         return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
     }
 
     public double getSalary() {
@@ -69,30 +46,8 @@ public class User {
         return city;
     }
 
-    public void setCity(String city) {
-    }
-
     public int getCompany() {
         return company;
-    }
-
-    public void setCompany(int company) {
-        this.company = company;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (id != user.id) return false;
-        if (Double.compare(user.salary, salary) != 0) return false;
-        if (name != null ? !name.equals(user.name) : user.name != null) return false;
-        if (sex != null ? !sex.equals(user.sex) : user.sex != null) return false;
-        if (city != null ? !city.equals(user.city) : user.city != null) return false;
-        return company != 0 ? company == user.company : user.company == 0;
     }
 
     @Override
