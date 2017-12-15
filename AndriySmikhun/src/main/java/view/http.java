eac37cc05ce57ca4c2dce86546100acf04aed582
smikhun <smikhun@gmail.com>
2017-12-16 00:55:15 +0200

    added new method for server2

--- AndriySmikhun/src/main/java/view/http.java
+++ AndriySmikhun/src/main/java/view/http.java
@@ -5,13 +5,14 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import com.google.gson.Gson;
-import sun.misc.IOUtils;
-import week3.model.Product;
+
+import week3.appdb.IappDB;
+import week3.appdb.IappDBimpl;
+import week3.model.Bill;
+import week3.model.Salesman;
 
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.stream.Collectors;
 
 
@@ -19,31 +20,31 @@ public class http {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8800), 0);
-        server.createContext("/index", new MyHandler());
+        server.createContext("/index", new Login());
+        server.createContext("/getbill", new GetBill());
         server.setExecutor(null);
         server.start();
         System.out.println("Server is runing");
-        //server.createContext("/hello", new myHello());
     }
 
-    static class MyHandler implements HttpHandler {
+    static class Login implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
 
             if (t.getRequestMethod().equals("POST")) {
-
-                System.out.println(t.getRequestURI());
-
                 String result = new BufferedReader(new InputStreamReader(t.getRequestBody()))
                         .lines().collect(Collectors.joining("\n"));
 
-                System.out.println(result);
+                result = result.replace("&","=");
+                String[] parameters = result.split("=");
+
+                String login = parameters[1];
+                String password = parameters[3];
 
                 Gson gson = new Gson();
-                Product pr = new Product(1, "JDK", 25.0);
-                InputStream stream = t.getRequestBody();
-                String istr = stream.toString();
-                String json = gson.toJson(pr);
+                Salesman user = new Salesman(1,"Andrii", "304FF76v", "Andrii Smikhun");
+
+                String json = gson.toJson(user);
                 t.sendResponseHeaders(200, json.length());
                 OutputStream os = t.getResponseBody();
                 os.write(json.getBytes());
@@ -52,5 +53,29 @@ public class http {
 
         }
     }
-   
+
+    static class GetBill implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+
+            if (t.getRequestMethod().equals("POST")) {
+                String result = new BufferedReader(new InputStreamReader(t.getRequestBody()))
+                        .lines().collect(Collectors.joining("\n"));
+
+
+                String[] parameters = result.split("=");
+                int index = Integer.parseInt(parameters[1]);
+                IappDB db = new IappDBimpl();
+                Bill bill = db.findeBillByID(index);
+                Gson gson = new Gson();
+                String json = gson.toJson(bill);
+                t.sendResponseHeaders(200, json.length());
+                OutputStream os = t.getResponseBody();
+                os.write(json.getBytes());
+                os.close();
+            }
+
+        }
+    }
+
 }
