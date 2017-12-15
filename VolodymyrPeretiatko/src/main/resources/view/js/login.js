985bde771f90fdd468fa6a259df0be8b2239a60b
vperetyatko <voc@ukr.net>
2017-12-15 13:17:39 +0200

    hw6

--- VolodymyrPeretiatko/src/main/resources/view/js/login.js
+++ VolodymyrPeretiatko/src/main/resources/view/js/login.js
@@ -1,10 +1,10 @@
-<script>
-    function sendLoginForm(){
+
+    function sendLoginForm() {
 
         var nameValue = document.getElementById("login").value;
         var passwordValue = document.getElementById("password").value;
 
-        var obj = {name : nameValue, password: passwordValue};
+        var keyValueObj = {name: nameValue, password: passwordValue};
 
         // construct an HTTP request
         var xhr = new XMLHttpRequest();
@@ -12,12 +12,11 @@
         xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
         // send the collected data as JSON
-        xhr.send(JSON.stringify(obj));
+        xhr.send(JSON.stringify(keyValueObj));
 
         xhr.onloadend = function (resp) {
-        // get access token and put in cookies
-        console.log("req has been sucessful\n" + resp);
+            // get access token and put in cookies
+            console.log("req has been sucessful\n" + resp)
+        };
         //window.location.replace("/view/html/user-page.html");
-        }
-    };
-</script>
+    }
